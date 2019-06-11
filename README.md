f6dda536760f5b8a8e7ec0149bdac3acf680ff55
Chris Vo <cvo@onica.com>
2019-06-11 00:02:22 -0700

    Added timer to the quiz.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Quiz Exercises
+
+## Links to refer to
+* https://c2fo.io/golang/2016/02/10/create-golang-timer/
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 type Question struct {
@@ -16,12 +18,35 @@ type Question struct {
 	CorrectIndicator bool   `json:"correctIndicator"`
 }
 
+const time_in_seconds = 8
+
 func main() {
+	// Parse flags
+	var userCsvFile = flag.String("csvFile", "problems.csv", "Defaults to problems.csv, pass flag if you want to use your own csv file.")
+	var timeInSeconds = flag.Int("timeInSeconds", 30, "Seconds that user has to take the quiz")
+
+	flag.Parse()
+
 	// Read in CSV file
-	csvFile, _ := os.Open("problems.csv")
+	csvFile, _ := os.Open(*userCsvFile)
 	reader := csv.NewReader(bufio.NewReader(csvFile))
 
-	var questionData []Question
+	// Declare correct or incorrect answer counter
+	correctAnswers := 0
+	incorrectAnswers := 0
+
+	// Set timer
+	timer := time.NewTimer(time.Duration(*timeInSeconds) * time.Second)
+
+	// Initiate
+	go func() {
+		<-timer.C
+		fmt.Printf("\n\n\nTimes up! Your %d second timer finished.", *timeInSeconds)
+		fmt.Println("\nCorrect answers:", correctAnswers)
+		fmt.Println("Incorrect answers:", incorrectAnswers)
+		os.Exit(0)
+	}()
+
 	// Iterate lines in CSV file.
 	for {
 		line, error := reader.Read()
@@ -35,22 +60,22 @@ func main() {
 		fmt.Printf("What is %s: ", line[0])
 		userInput, _ := userInputReader.ReadString('\n')
 		if strings.TrimSpace(userInput) == line[1] {
-			fmt.Println("Correct")
-			questionData = append(questionData, Question{
-				Question:         line[0],
-				Answer:           line[1],
-				CorrectIndicator: true,
-			})
+			fmt.Println("Correct\n")
+			// Increment correctAnswers variable
+			correctAnswers += 1
+
 		} else {
-			fmt.Printf("Incorrect. Answer is %s. You typed %s", line[1], userInput)
-			questionData = append(questionData, Question{
-				Question:         line[0],
-				Answer:           line[1],
-				CorrectIndicator: false,
-			})
+			fmt.Printf("Incorrect. Answer is %s. You typed %s\n", line[1], userInput)
+			// Increment incorrectAnswers
+			incorrectAnswers += 1
 		}
-		// fmt.Println(line[0], line[1])
 	}
+	stop := timer.Stop()
+	if stop {
+		fmt.Println("You ran out of time")
+	}
+	fmt.Println("\nCorrect answers:", correctAnswers)
+	fmt.Println("Incorrect answers:", incorrectAnswers)
 }
 
 // questionJson, _ := json.Marshal(question)
